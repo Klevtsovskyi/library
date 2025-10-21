e57ce82c5fa1d88dcd778878398be4a76050b319
Arsen <avklevtsovskiy@gmail.com>
2025-10-21 12:43:28 +0300

    add models

--- app/catalog/models.py
+++ app/catalog/models.py
@@ -1,3 +1,48 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.RESTRICT)
+    summary = models.TextField(max_length=2000, blank=True, null=True)
+    isbn = models.CharField(max_length=13, blank=True, null=True)
+    genre = models.ManyToManyField('Genre', blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class BookInstance(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ("a", "Доступно"),
+        ("r", "Зарезервовано"),
+        ("m", "На обслуговуванні"),
+        ("o", "Орендована")
+    )
+
+    status = models.CharField(max_length=1, choices=LOAN_STATUS, default="a")
+    book = models.ForeignKey(Book, on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return self.book.title + ' ' + self.id

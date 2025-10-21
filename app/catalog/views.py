886759767f633c31c00129bf98af339dc60a654a
Arsen <avklevtsovskiy@gmail.com>
2025-10-21 13:27:57 +0300

    update index

--- app/catalog/views.py
+++ app/catalog/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 
+from .models import Book, Author, BookInstance
+
 
 def index(request):
-    return render(request, 'catalog/index.html')
+    book_num = Book.objects.all().count()
+    author_num = Author.objects.all().count()
+    bookinstance_num = BookInstance.objects.all().count()
+    bookinstance_available_num = BookInstance.objects.filter(status__exact="a").count()
+    context = {
+        'book_num': book_num,
+        'author_num': author_num,
+        'bookinstance_num': bookinstance_num,
+        'bookinstance_available_num': bookinstance_available_num,
+    }
+    return render(request, 'catalog/index.html', context)
 

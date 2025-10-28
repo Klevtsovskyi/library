b8c8849cf0195c2297ac9206ff549993aeeb0623
Arsen <avklevtsovskiy@gmail.com>
2025-10-28 12:40:53 +0200

    update settings.py

--- app/library/settings.py
+++ app/library/settings.py
@@ -109,6 +109,13 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+AUTH_USER_MODEL = "auth.User"
+
+LOGIN_URL = "/accounts/login/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/accounts/login/"
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 

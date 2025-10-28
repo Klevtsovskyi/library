9792aab6488d51bc34951d70d0a126fe498ff8fc
Arsen <avklevtsovskiy@gmail.com>
2025-10-28 13:10:14 +0200

    add auth

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import Group
+from django.contrib import messages
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def registration(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            reader = Group.objects.get(name='Reader')
+            user.groups.add(reader)
+            return redirect('login')
+        else:
+            messages.error(request, "Дані не валідні!")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "accounts/registration.html", {'form': form})
+
